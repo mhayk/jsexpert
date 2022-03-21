a4ba59de9c5ef9787a5e5bb9ed57bbab9a4cc114
Mhayk Whandson <hi@mhayk.com>
2022-03-21 21:44:05 +0000

    WIP
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ class01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    error: {
+        FILE_LENGTH_ERROR_MESSAGE: "File content length is invalid!",
+        FILE_FIELDS_ERROR_MESSAGE: "File provided properties are invalid!",
+    }
+}
+
+module.exports = constants
---
+++ class01-mocks/src/file.js
@@ -0,0 +1,51 @@
+const { readFile } = require('fs/promises');
+const { join } = require('path')
+const { error } = require('./constants')
+
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: ["id", "name", "profession", "age"]
+}
+class File {
+    static async csvToJson(filePath) {
+        const content = await File.getFileContent(filePath);
+        const validation = File.isValid(content);
+        if (!validation.valid) throw new Error(validation.error);
+
+        return content;
+    }
+
+    static async getFileContent(filePath) {
+        const filename = join(__dirname, filePath);
+        return (await readFile(filename)).toString("utf8");
+    }
+
+    static isValid(cvsString, options = DEFAULT_OPTION) {
+        const [header, ...fileWithoutHeader] = cvsString.split("\n");
+        const isHeaderValid = header === options.fields.join(",");
+
+        if (!isHeaderValid) {
+            return {
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        const isContentLengthAccepted = (
+            fileWithoutHeader.length > 0 &&
+            fileWithoutHeader.length <= options.maxLines
+        )
+
+        if (!isContentLengthAccepted) {
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        return { valid: true }
+    }
+}
+
+module.exports = File
+
